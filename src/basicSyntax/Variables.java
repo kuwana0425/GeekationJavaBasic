eea721bc41e58883a966158a8ba4604e9733bd34
Kuwana0425 <y.kuwana.cingroup2012@gmail.com>
2024-06-24 13:23:46 +0900

    変数の実装

--- src/basicSyntax/Variables.java
+++ src/basicSyntax/Variables.java
@@ -6,16 +6,16 @@ public class Variables {
 		String subject = "私の";
 		String adjective = "好きな";
 		String verb ="です。";
-	    
-	    int num1;
-	    double num2;
-	    
-	    num1= 1;
-	    num2= 0.2;
-	    
-	    System.out.println(num1);
-	    System.out.println(num2);
-	    System.out.println(subject + adjective +  "整数は" + num1 + verb);
-	    System.out.println(subject + adjective +  "少数は" + num2 + verb);
+		
+		int num1;
+		double num2;
+		
+		num1= 1;
+		num2= 0.2;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(subject + adjective +  "整数は" + num1 + verb);
+		System.out.println(subject + adjective +  "少数は" + num2 + verb);
 	}
 }
