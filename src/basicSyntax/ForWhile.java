bab5b3df317f1757ce1ba3a0043d33b990de2319
Kuwana0425 <y.kuwana.cingroup2012@gmail.com>
2024-07-01 17:23:32 +0900

    繰り返し処理 実装

---
+++ src/basicSyntax/ForWhile.java
@@ -0,0 +1,34 @@
+package basicSyntax;
+
+public class ForWhile {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i < 100) {
+		i++;
+		if (i % 15 == 0) {
+			System.out.println("FizzBuzz");
+		} else if (i % 3 == 0) {
+			System.out.println("Fizz");
+		} else if (i % 5 == 0) {
+			System.out.println("Buzz");
+		} else {
+			System.out.println(i);
+		}
+	}
+	     String [] languages = {"Ruby", "Golang", "Java", "Python", "Typescript", "HTML", "Javascript"};
+	     
+	     for (String language : languages ) {
+	     if(language == "Java") {
+	    	continue;
+	     }
+	       System.out.println("現在学習中の言語は" + language + "です");
+	     }
+	     for (String language : languages ) {
+	     if(language == "HTML") {
+	       System.out.println(language + "はプログラミング言語ではありません。");
+	        break;
+	      }
+	   }
+	}
+}
