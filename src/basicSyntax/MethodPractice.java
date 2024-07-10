26e472140f2ae93396915e7021a7c3069571c3a7
Kuwana0425 <y.kuwana.cingroup2012@gmail.com>
2024-07-10 23:06:40 +0900

    メソッド実装　再々修正

--- src/basicSyntax/MethodPractice.java
+++ src/basicSyntax/MethodPractice.java
@@ -1,37 +1,32 @@
 package basicSyntax;
 public class MethodPractice {
 	public static void main(String[] args) {
-	int result1 =add(10,5);
-	int result2 =sub(10,5);
-	int result3 =mult(10,5);
-	int result4 =div(10,5);
-	
-	System.out.println("計算結果は"+result1+"です。");
-	System.out.println("計算結果は"+result2+"です。");
-	System.out.println("計算結果は"+result3+"です。");
-	System.out.println("計算結果は"+result4+"です。");
-}
-	private static int add(int a, int b) {
-		int x = a + b;
+		int a = 10;
+		int b = 5;
 		
-		return x;
-	}
-	
-	private static int sub(int a, int b) {
-		int x = a - b;
+		int result1 = add(a, b);
+		int result2 = sub(a, b);
+		int result3 = mult(a, b);
+		int result4 = div(a, b);
 		
-		return x;
-	}
-	
-	private static int mult(int a, int b) {
-		int x = a * b;
-		
-		return x;
-	}
-	
-	private static int div(int a, int b) {
-		int x = a / b;
-		
-		return x;
+		printAllPrice(a, result1,result2,result3,result4);
+		}
+	public static int add(int a, int b) {
+		return a + b;
+		}
+	public static int sub(int a, int b) {
+		return a - b;
+		}
+	public static int mult(int a, int b) {
+		return a * b;
+		}
+	public static int div(int a, int b) {
+		return a / b;
+		}
+	public static void printAllPrice(int a, int result1,int result2,int result3,int result4) {
+		System.out.println("計算結果は " + result1 + "です。");
+		System.out.println("計算結果は " + result2 + "です。");
+		System.out.println("計算結果は " + result3 + "です。");
+		System.out.println("計算結果は " + result4 + "です。");
 	}
 }
