7f636d733c77ef108fd9089f032d371188941bad
Kuwana0425 <y.kuwana.cingroup2012@gmail.com>
2024-07-12 17:16:43 +0900

    メソッド　実装　再々々修正

--- src/basicSyntax/MethodPractice.java
+++ src/basicSyntax/MethodPractice.java
@@ -1,32 +1,33 @@
 package basicSyntax;
 public class MethodPractice {
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 5;
+		int result1 = add(10,5);
+		int result2 = sub(10,5);
+		int result3 = mult(10,5);
+		int result4 = div(10,5);
 		
-		int result1 = add(a, b);
-		int result2 = sub(a, b);
-		int result3 = mult(a, b);
-		int result4 = div(a, b);
-		
-		printAllPrice(a, result1,result2,result3,result4);
-		}
+		calculation(result1);
+		calculation(result2);
+		calculation(result3);
+		calculation(result4);
+	}
 	public static int add(int a, int b) {
-		return a + b;
+		int result = a + b;
+		return  result;
 		}
 	public static int sub(int a, int b) {
-		return a - b;
+		int result = a - b;
+		return result;
 		}
 	public static int mult(int a, int b) {
-		return a * b;
+		int result = a * b;
+		return result;
 		}
 	public static int div(int a, int b) {
-		return a / b;
+		int result =a / b;
+		return result;
 		}
-	public static void printAllPrice(int a, int result1,int result2,int result3,int result4) {
-		System.out.println("計算結果は " + result1 + "です。");
-		System.out.println("計算結果は " + result2 + "です。");
-		System.out.println("計算結果は " + result3 + "です。");
-		System.out.println("計算結果は " + result4 + "です。");
+	public static void calculation(int result) {
+		System.out.println("計算結果は " + result + "です。");
 	}
 }
