d8a1a9a435e3b4db52a08c2c65f2f0a0812cb66f
Kuwana0425 <y.kuwana.cingroup2012@gmail.com>
2024-07-17 15:58:21 +0900

    クラス　実装

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -3,7 +3,13 @@ package basicSyntax;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Geekation");
+		int age = 10;
+		double height  = 150;
+		
+		GeekIntroduction.greeting(age,height);
+		
+		int num1 = 1;
+		int num2 = 50;
+		GeekIntroduction.specialSkill(num1,num2);
+		}
 	}
-
-}
