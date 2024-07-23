de18191540462e23ca9d85f218b1a821a12d7d38
Kuwana0425 <y.kuwana.cingroup2012@gmail.com>
2024-07-23 15:09:31 +0900

    クラス　再々修正

--- src/basicSyntax/GeekIntroduction.java
+++ src/basicSyntax/GeekIntroduction.java
@@ -10,35 +10,28 @@ public static void greeting(int age,double height){
 
 public static void specialSkill(int num1,int num2) {
 	
-	while (num1 < num2){
-		System.out.println(num1);
-		num1++;
-		
+	while (num1 <= num2){
 	if (num1 < 0 || num2 < 0) {
 	System.out.println("num1とnum2は0より大きい値にしてください。");
-	num1++;
 	break;
 	} else if (num1 >= num2) {
 	System.out.println("num2はnum1より大きい値にしてください。");
-	num1++;
 	break;
 	} else if (num1 > 300 || num2 > 300) {
 	System.out.println("num1とnum2は300以下にしてください。");
-	num1++;
 	break;
 	}else if (num1 % 15 == 0) {
 	System.out.println(num1+"は3の倍数かつ5の倍数です。");
-	num1++;
-	continue;
+	
 	}else if (num1 % 3 ==0) {
 	System.out.println(num1+"は3の倍数です。");
-	num1++;
-	continue;
+	
 	}else if (num1 % 5 ==0) {
 	System.out.println(num1+"は5の倍数です。");
-	num1++;
-	continue;
-	}
+
+	}else
+	System.out.println(num1);
+	num1++;	
 }
 }
 }
--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -5,11 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		int age = 10;
 		double height  = 150;
-		
+
 		GeekIntroduction.greeting(age,height);
-		
+
 		int num1 = 1;
 		int num2 = 50;
 		GeekIntroduction.specialSkill(num1,num2);
+		
 		}
 	}
